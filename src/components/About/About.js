67467c687b7c95bfb7676bc9c164558df00e87de
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-05-02 12:38:52 -0700

    Updated Logo to link to About Page

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+const About=()=>{
+    return(
+        <div>
+        <h1>The Magic Face Recongition App</h1>
+        <h2>How to use</h2>
+        <p>
+            To get started using the Magic Face
+        </p>
+        </div>
+    );
+}
+
+export default About; 
