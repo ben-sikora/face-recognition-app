c6f358b91eae28b49364960c5b2a62093bff3061
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-05-02 13:05:40 -0700

    Update About content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,21 @@
 import React from 'react'; 
-
+import './About.css';
 const About=()=>{
     return(
-        <div>
+        <div className='w-100 flex justify-center'>
+        <div className='w-60'>
         <h1>The Magic Face Recongition App</h1>
         <h2>How to use</h2>
         <p>
-            To get started using the Magic Face
+            To get started using the Magic Face Recongition App first register! Enter an email, name, and passowrd to get started. As this is one of my first full-stack projects, I would recommend using fake information.
         </p>
+        <p>
+            Find a link of any image online and enter it into the searhbox. Click submit and watch as AI magic finds an identifies the face in your image!
+        </p>
+        <p>
+            Watch your score run up and compete against your friends!
+        </p>
+        </div>
         </div>
     );
 }
